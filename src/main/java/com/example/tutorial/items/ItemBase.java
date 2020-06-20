cfcf2dd4339ec7d3fc93ee652e4895cf28741f43
TheOnlyTails <shacharzidon@gmail.com>
2020-06-20 12:30:38 +0300

    Initial commit

---
+++ src/main/java/com/example/tutorial/items/ItemBase.java
@@ -0,0 +1,10 @@
+package com.example.tutorial.items;
+
+import com.example.tutorial.TheOnlyTails;
+import net.minecraft.item.Item;
+
+public class ItemBase extends Item {
+    public ItemBase() {
+        super(new Item.Properties().group(TheOnlyTails.TAB));
+    }
+}
