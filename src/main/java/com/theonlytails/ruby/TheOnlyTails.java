cfc33341e7ca83f6907f07a0f2919e3dad69e39c
TheOnlyTails <shacharzidon@gmail.com>
2020-07-27 17:56:00 +0300

    Added custom dispenser behaviors for the ruby sheep spawn egg;

--- src/main/java/com/theonlytails/ruby/TheOnlyTails.java
+++ src/main/java/com/theonlytails/ruby/TheOnlyTails.java
@@ -2,20 +2,25 @@ package com.theonlytails.ruby;
 
 import com.theonlytails.ruby.entities.RubySheepEntity;
 import com.theonlytails.ruby.init.*;
-import net.minecraft.client.renderer.entity.SheepRenderer;
-import net.minecraft.client.renderer.entity.model.SheepModel;
+import com.theonlytails.ruby.items.RubySheepSpawnEgg;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.Direction;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DeferredWorkQueue;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 @Mod("ruby")
 public class TheOnlyTails {
@@ -47,6 +52,22 @@ public class TheOnlyTails {
 
     private void setup(final FMLCommonSetupEvent event) {
         DeferredWorkQueue.runLater(() -> GlobalEntityTypeAttributes.put(RubyEntityTypes.RUBY_SHEEP.get(), RubySheepEntity.setCustomAttributes().create()));
+        DeferredWorkQueue.runLater(() -> DispenserBlock.registerDispenseBehavior(ItemsRegistry.RUBY_SHEEP_SPAWN_EGG::get,
+                new DefaultDispenseItemBehavior() {
+                    public @NotNull ItemStack dispenseStack(@NotNull IBlockSource source, @NotNull ItemStack stack) {
+                        Direction direction = source.getBlockState().get(DispenserBlock.FACING);
+                        ((RubySheepSpawnEgg) stack.getItem()).getType(stack.getTag()).spawn(source.getWorld(),
+                                stack,
+                                null,
+                                source.getBlockPos().offset(direction),
+                                SpawnReason.DISPENSER,
+                                direction != Direction.UP,
+                                false);
+                        stack.shrink(1);
+                        return stack;
+                    }
+                }
+        ));
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
