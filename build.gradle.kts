d02783f4678cb80b7b5d14d562c0e01b0bd7832c
TheOnlyTails <65342367+TheOnlyTails@users.noreply.github.com>
2021-03-30 01:02:25 +0300

    Switched from the default loot table creation system to the LootTables DSL system. (#74)
    * Switched from the default loot table creation system to the LootTables DSL system.
    
    Signed-off-by: TheOnlyTails <shacharzidon@gmail.com>
    
    * Fixed a problem where loot entries being generated by the LootTables DSL were being added twice.
    Fixed a problem with the dependency.
    
    Signed-off-by: TheOnlyTails <shacharzidon@gmail.com>
    
    * Minor changes in build.gradle.kts.
    
    Signed-off-by: TheOnlyTails <shacharzidon@gmail.com>
    
    * Updated LootTables.
    
    Signed-off-by: TheOnlyTails <shacharzidon@gmail.com>
    
    * Fixed a problem with the centrifuge's blockstates file.
    
    Signed-off-by: TheOnlyTails <shacharzidon@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 import Build_gradle.*
+import net.minecraftforge.gradle.userdev.DependencyManagementExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 // BuildScript
 buildscript {
-    repositories {
-        maven(url = "https://files.minecraftforge.net/maven")
-        jcenter()
-        mavenCentral()
-    }
+	repositories {
+		maven(url = "https://files.minecraftforge.net/maven")
+		jcenter()
+		mavenCentral()
+	}
 
-    dependencies {
-        classpath(group = "net.minecraftforge.gradle", name = "ForgeGradle", version = "4.1.+")
-        classpath(group = "org.jetbrains.kotlin", name = "kotlin-gradle-plugin", version = "1.4.31")
-    }
+	dependencies {
+		classpath(group = "net.minecraftforge.gradle", name = "ForgeGradle", version = "4.1.+")
+		classpath(group = "org.jetbrains.kotlin", name = "kotlin-gradle-plugin", version = "1.4.31")
+	}
 }
 
 // Config -> Minecraft
@@ -37,57 +38,66 @@ val archivesBaseName: String by extra
 
 // Plugins
 plugins {
-    `java-library`
-    kotlin("jvm") version ("1.4.31")
+	`java-library`
+	kotlin("jvm") version ("1.4.31")
 }
 
 apply(plugin = "net.minecraftforge.gradle")
 
 // JVM Info
 println(
-    "Java: ${System.getProperty("java.version")}" +
-            " JVM: ${System.getProperty("java.vm.version")}(${System.getProperty("java.vendor")})" +
-            " Arch: ${System.getProperty("os.arch")}"
+	"Java: ${System.getProperty("java.version")}" +
+			" JVM: ${System.getProperty("java.vm.version")}(${System.getProperty("java.vendor")})" +
+			" Arch: ${System.getProperty("os.arch")}"
 )
 
 repositories {
-    maven {
-        name = "kotlinforforge"
-        url = uri("https://thedarkcolour.github.io/KotlinForForge/")
-    }
+	maven {
+		name = "kotlinforforge"
+		url = uri("https://thedarkcolour.github.io/KotlinForForge/")
+	}
+
+	maven(url = "https://jitpack.io")
 }
 
 // Note: Due to the way kotlin gradle works we need to define the minecraft dependency before we configure Minecraft
 dependencies {
-    "minecraft"(group = "net.minecraftforge", name = "forge", version = "$minecraftVersion-$forgeVersion")
+	"minecraft"(group = "net.minecraftforge", name = "forge", version = "$minecraftVersion-$forgeVersion")
 
-    implementation(group = "thedarkcolour", name = "kotlinforforge", version = kffVersion)
+	implementation(group = "thedarkcolour", name = "kotlinforforge", version = kffVersion)
+
+	implementation(project.the<DependencyManagementExtension>()
+		.deobf(project.dependencies.create(group = "com.github.TheOnlyTails", name = "LootTables", version = "0.2.3")
+			.apply {
+				isTransitive = false
+			}
+		))
 }
 
 // Minecraft
 minecraft {
-    mappingChannel = mappingsChannel
-    mappingVersion = minecraftVersion
+	mappingChannel = mappingsChannel
+	mappingVersion = minecraftVersion
 
-    accessTransformer(file("src/main/resources/META-INF/accesstransformer.cfg"))
+	accessTransformer(file("src/main/resources/META-INF/accesstransformer.cfg"))
 
-    runs {
-        config("client")
+	runs {
+		config("client")
 
-        config("server")
+		config("server")
 
-        config("data") {
-            args(
-                "--mod",
-                modId,
-                "--all",
-                "--output",
-                file("src/generated/resources/"),
-                "--existing",
-                file("src/main/resources/")
-            )
-        }
-    }
+		config("data") {
+			args(
+				"--mod",
+				modId,
+				"--all",
+				"--output",
+				file("src/generated/resources/"),
+				"--existing",
+				file("src/main/resources/")
+			)
+		}
+	}
 }
 
 // Setup
@@ -97,13 +107,13 @@ base.archivesBaseName = archivesBaseName
 
 // Java 8 Target
 tasks.withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+	sourceCompatibility = "1.8"
+	targetCompatibility = "1.8"
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = "1.8"
-    kotlinOptions.useIR = true
+	kotlinOptions.jvmTarget = "1.8"
+	kotlinOptions.useIR = true
 }
 
 // Finalize the jar by Reobf
@@ -111,17 +121,17 @@ tasks.named<Jar>("jar") { finalizedBy("reobfJar") }
 
 // Manifest
 tasks.withType<Jar> {
-    manifest {
-        attributes(
-            "Specification-Title" to modId,
-            "Specification-Vendor" to author,
-            "Specification-Version" to "1",
-            "Implementation-Title" to project.name,
-            "Implementation-Version" to project.version,
-            "Implementation-Vendor" to author,
-            "Implementation-Timestamp" to Date().format("yyyy-MM-dd'T'HH:mm:ssZ")
-        )
-    }
+	manifest {
+		attributes(
+			"Specification-Title" to modId,
+			"Specification-Vendor" to author,
+			"Specification-Version" to "1",
+			"Implementation-Title" to project.name,
+			"Implementation-Version" to project.version,
+			"Implementation-Vendor" to author,
+			"Implementation-Timestamp" to Date().format("yyyy-MM-dd'T'HH:mm:ssZ")
+		)
+	}
 }
 
 // Generated Resources
@@ -140,19 +150,19 @@ typealias UserDevExtension = net.minecraftforge.gradle.userdev.UserDevExtension
 typealias RunConfiguration = RunConfig.() -> Unit
 
 fun minecraft(configuration: UserDevExtension.() -> Unit) =
-    configuration(extensions.getByName("minecraft") as UserDevExtension)
+	configuration(extensions.getByName("minecraft") as UserDevExtension)
 
 fun NamedDomainObjectContainerScope<RunConfig>.config(name: String, additionalConfiguration: RunConfiguration = {}) {
-    val runDirectory = project.file("run")
-    val sourceSet = the<JavaPluginConvention>().sourceSets["main"]
+	val runDirectory = project.file("run")
+	val sourceSet = the<JavaPluginConvention>().sourceSets["main"]
 
-    create(name) {
-        workingDirectory(runDirectory)
-        property("forge.logging.markers", markers)
-        property("forge.logging.console.level", level)
+	create(name) {
+		workingDirectory(runDirectory)
+		property("forge.logging.markers", markers)
+		property("forge.logging.console.level", level)
 
-        additionalConfiguration(this)
+		additionalConfiguration(this)
 
-        mods { create(modId) { source(sourceSet) } }
-    }
+		mods { create(modId) { source(sourceSet) } }
+	}
 }
--- src/main/kotlin/com/theonlytails/rubymod/datagen/BlockLootTablesGenerator.kt
+++ src/main/kotlin/com/theonlytails/rubymod/datagen/BlockLootTablesGenerator.kt
@@ -1,27 +1,17 @@
 package com.theonlytails.rubymod.datagen
 
+import com.theonlytails.loottables.*
 import com.theonlytails.rubymod.logger
 import com.theonlytails.rubymod.registries.BlockRegistry
 import com.theonlytails.rubymod.registries.ItemRegistry
-import net.minecraft.advancements.criterion.EnchantmentPredicate
-import net.minecraft.advancements.criterion.ItemPredicate
-import net.minecraft.advancements.criterion.MinMaxBounds
-import net.minecraft.advancements.criterion.StatePropertiesPredicate
 import net.minecraft.block.Block
 import net.minecraft.block.SlabBlock
-import net.minecraft.data.DataGenerator
-import net.minecraft.data.DirectoryCache
-import net.minecraft.data.IDataProvider
-import net.minecraft.data.LootTableProvider
+import net.minecraft.data.*
 import net.minecraft.enchantment.Enchantments
-import net.minecraft.loot.*
-import net.minecraft.loot.conditions.BlockStateProperty
-import net.minecraft.loot.conditions.MatchTool
-import net.minecraft.loot.conditions.SurvivesExplosion
-import net.minecraft.loot.functions.ApplyBonus
+import net.minecraft.loot.LootParameterSets
+import net.minecraft.loot.LootTable
+import net.minecraft.loot.LootTableManager
 import net.minecraft.loot.functions.CopyName
-import net.minecraft.loot.functions.ExplosionDecay
-import net.minecraft.loot.functions.SetCount
 import net.minecraft.state.properties.SlabType
 import net.minecraft.util.ResourceLocation
 
@@ -31,150 +21,142 @@ import net.minecraft.util.ResourceLocation
  * @author TheOnlyTails
  */
 class BlockLootTablesGenerator(private val generator: DataGenerator) : LootTableProvider(generator) {
-    private val tables = hashMapOf<Block, LootTable.Builder>()
+	private val tables = hashMapOf<Block, LootTable>()
 
-    private fun addLootTables(loot: BlockLootTablesGenerator) {
-        // Centrifuge
-        loot.addLoot(
-            BlockRegistry.centrifuge, LootTable.lootTable()
-                .withPool(
-                    LootPool.lootPool()
-                        .add(
-                            ItemLootEntry.lootTableItem(ItemRegistry.centrifuge)
-                                .`when`(SILK_TOUCH)
-                                .otherwise(ItemLootEntry.lootTableItem(ItemRegistry.ruby))
-                        )
-                        .`when`(SurvivesExplosion.survivesExplosion())
-                )
-        )
+	private fun addLootTables(loot: BlockLootTablesGenerator) {
+		// Centrifuge
+		loot.addLoot(
+			BlockRegistry.centrifuge, lootTable(LootParameterSets.BLOCK) {
+				pool {
+					alternativesEntry(
+						itemEntry(ItemRegistry.centrifuge) {
+							condition { hasSilkTouch() }
+						},
+						itemEntry(ItemRegistry.ruby),
+					)
 
-        // Ruby Barrel
-        loot.addLoot(
-            BlockRegistry.rubyBarrel, LootTable.lootTable()
-                .withPool(
-                    LootPool.lootPool()
-                        .add(
-                            ItemLootEntry.lootTableItem(ItemRegistry.rubyBarrel)
-                                .apply(CopyName.copyName(CopyName.Source.BLOCK_ENTITY))
-                        )
-                        .`when`(SurvivesExplosion.survivesExplosion())
-                )
-        )
+					condition { survivesExplosion() }
+				}
+			}
+		)
 
-        // Ruby Block
-        loot.dropSelf(BlockRegistry.rubyBlock)
+		// Ruby Barrel
+		loot.addLoot(BlockRegistry.rubyBarrel, lootTable(LootParameterSets.BLOCK) {
+			pool {
+				itemEntry(BlockRegistry.rubyBarrel) {
+					function { copyName(CopyName.Source.BLOCK_ENTITY) }
+				}
 
-        // Ruby Slab
-        loot.dropSlabs(BlockRegistry.rubySlab)
+				condition { survivesExplosion() }
+			}
+		})
 
-        // Ruby Stairs
-        loot.dropSelf(BlockRegistry.rubyStairs)
+		// Ruby Block
+		loot.dropSelf(BlockRegistry.rubyBlock)
 
-        // Ruby Pressure Plate
-        loot.dropSelf(BlockRegistry.rubyPressurePlate)
+		// Ruby Slab
+		loot.dropSlabs(BlockRegistry.rubySlab)
 
-        // Ruby Button
-        loot.dropSelf(BlockRegistry.rubyButton)
+		// Ruby Stairs
+		loot.dropSelf(BlockRegistry.rubyStairs)
 
-        // Ruby Wall
-        loot.dropSelf(BlockRegistry.rubyWall)
+		// Ruby Pressure Plate
+		loot.dropSelf(BlockRegistry.rubyPressurePlate)
 
-        // Ruby Carpet
-        loot.dropSelf(BlockRegistry.rubyCarpet)
+		// Ruby Button
+		loot.dropSelf(BlockRegistry.rubyButton)
 
-        // Logic Gate
-        loot.dropSelf(BlockRegistry.logicGate)
+		// Ruby Wall
+		loot.dropSelf(BlockRegistry.rubyWall)
 
-        // Ruby Ore
-        loot.addLoot(
-            BlockRegistry.rubyOre, LootTable.lootTable()
-                .withPool(
-                    LootPool.lootPool()
-                        .add(
-                            ItemLootEntry.lootTableItem(ItemRegistry.rubyOre)
-                                .`when`(SILK_TOUCH)
-                                .otherwise(
-                                    ItemLootEntry.lootTableItem(ItemRegistry.ruby)
-                                        .apply(ExplosionDecay.explosionDecay())
-                                        .apply(ApplyBonus.addOreBonusCount(Enchantments.BLOCK_FORTUNE))
-                                )
-                        )
-                        .`when`(SurvivesExplosion.survivesExplosion())
-                )
-        )
+		// Ruby Carpet
+		loot.dropSelf(BlockRegistry.rubyCarpet)
 
-        // Ruby Wool
-        loot.dropSelf(BlockRegistry.rubyWool)
-    }
+		// Logic Gate
+		loot.dropSelf(BlockRegistry.logicGate)
 
-    // Always drop, unless explosion
-    private fun dropSelf(block: Block) {
-        val pool = LootPool.lootPool()
-            .setRolls(ConstantRange.exactly(1))
-            .add(ItemLootEntry.lootTableItem(block))
-            .`when`(SurvivesExplosion.survivesExplosion())
+		// Ruby Ore
+		loot.addLoot(BlockRegistry.rubyOre, lootTable(LootParameterSets.BLOCK) {
+			pool {
+				alternativesEntry(
+					itemEntry(ItemRegistry.rubyOre) {
+						condition { hasSilkTouch() }
+					},
+					itemEntry(ItemRegistry.ruby) {
+						function { explosionDecay() }
+						function { oreBonusCount(Enchantments.BLOCK_FORTUNE) }
+					},
+				)
 
-        tables[block] = LootTable.lootTable().withPool(pool)
-    }
+				condition { survivesExplosion() }
+			}
+		})
 
-    private fun dropSlabs(block: Block) {
-        val pool = LootPool.lootPool()
-            .setRolls(ConstantRange.exactly(1))
-            .add(ItemLootEntry.lootTableItem(block))
-            .`when`(SurvivesExplosion.survivesExplosion())
-            .apply(
-                SetCount.setCount(ConstantRange.exactly(2))
-                    .`when`(
-                        BlockStateProperty.hasBlockStateProperties(block)
-                            .setProperties(
-                                StatePropertiesPredicate.Builder.properties()
-                                    .hasProperty(SlabBlock.TYPE, SlabType.DOUBLE)
-                            )
-                    )
-            )
+		// Ruby Wool
+		loot.dropSelf(BlockRegistry.rubyWool)
+	}
 
-        tables[block] = LootTable.lootTable().withPool(pool)
-    }
+	// Always drop, unless explosion
+	private fun dropSelf(block: Block) {
+		tables[block] = lootTable(LootParameterSets.BLOCK) {
+			pool {
+				itemEntry(block)
+				condition { survivesExplosion() }
+			}
+		}
+	}
 
-    // Add a custom loot table
-    private fun addLoot(block: Block, loot: LootTable.Builder) {
-        tables[block] = loot
-    }
+	private fun dropSlabs(block: Block) {
+		tables[block] = lootTable(LootParameterSets.BLOCK) {
+			pool {
+				itemEntry(block)
 
-    /**
-     * Performs this provider's action.
-     */
-    override fun run(cache: DirectoryCache) {
-        addLootTables(this)
+				condition { survivesExplosion() }
 
-        val namespacedTables = hashMapOf<ResourceLocation, LootTable>()
+				function {
+					setConstantCount(2) {
+						condition {
+							blockStateProperty(block) {
+								setProperties(stateProperties { hasProperty(SlabBlock.TYPE, SlabType.DOUBLE) })
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 
-        for (entry in tables) {
-            namespacedTables[entry.key.lootTable] = entry.value.setParamSet(LootParameterSets.BLOCK).build()
-        }
+	// Add a custom loot table
+	private fun addLoot(block: Block, loot: LootTable) {
+		tables[block] = loot
+	}
 
-        writeLootTables(namespacedTables, cache)
-    }
+	/**
+	 * Performs this provider's action.
+	 */
+	override fun run(cache: DirectoryCache) {
+		addLootTables(this)
 
-    private fun writeLootTables(tables: Map<ResourceLocation, LootTable>, cache: DirectoryCache) {
-        val output = generator.outputFolder
+		val namespacedTables = hashMapOf<ResourceLocation, LootTable>()
 
-        tables.forEach { (key, table) ->
-            val path = output.resolve("data/${key.namespace}/loot_tables/${key.path}.json")
+		for (entry in tables) {
+			namespacedTables[entry.key.lootTable] = entry.value
+		}
 
-            try {
-                IDataProvider.save(gson, cache, LootTableManager.serialize(table), path)
-            } catch (e: Exception) {
-                logger.error("Couldn't write loot table $path", e)
-            }
-        }
-    }
+		writeLootTables(namespacedTables, cache)
+	}
 
-    companion object {
-        // Templates
-        private val SILK_TOUCH = MatchTool.toolMatches(
-            ItemPredicate.Builder.item()
-                .hasEnchantment(EnchantmentPredicate(Enchantments.SILK_TOUCH, MinMaxBounds.IntBound.atLeast(1)))
-        )
-    }
+	private fun writeLootTables(tables: Map<ResourceLocation, LootTable>, cache: DirectoryCache) {
+		val output = generator.outputFolder
+
+		tables.forEach { (key, table) ->
+			val path = output.resolve("data/${key.namespace}/loot_tables/${key.path}.json")
+
+			try {
+				IDataProvider.save(gson, cache, LootTableManager.serialize(table), path)
+			} catch (e: Exception) {
+				logger.error("Couldn't write loot table $path", e)
+			}
+		}
+	}
 }
